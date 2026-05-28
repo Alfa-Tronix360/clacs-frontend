504320f2b83282bc8a296268f914f91916c1eb82
Alfa-Tronix360 <afonsorogerio369@gmail.com>
2026-05-28 22:05:04 +0100

    fix api url render

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = `http://localhost:8001`;
+const API_BASE_URL = `https://clacs-backend.onrender.com`;
 
 // Obter token de autenticação do localStorage
 function getAuthToken() {
